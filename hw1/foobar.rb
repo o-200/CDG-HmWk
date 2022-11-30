2897efc481b120dc53a9895ebfd37a5023480c11
Alexander Abramov <omgout200@gmail.com>
2022-11-30 22:30:14 +0700

    completed

---
+++ hw1/foobar.rb
@@ -0,0 +1,3 @@
+def foobar(num1, num2)
+  num1 == 20 || num2 == 20 ? num2 : num1 + num2
+end
--- hw1/greeting.rb
+++ hw1/greeting.rb
@@ -5,7 +5,3 @@ def greeting(firstname, secondname, age)
     "Привет, #{firstname} #{secondname}. Тебе меньше 18 лет, но начать учиться программировать никогда не рано"
   end
 end
-
-def foobar(num1, num2)
-  num1 == 20 || num2 == 20 ? num2 : num1 + num2
-end
