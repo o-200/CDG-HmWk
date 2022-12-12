9983871bcb2ef893a7ebcf22c35ef11b197401db
Alexander Abramov <omgout200@gmail.com>
2022-12-12 15:23:01 +0700

    added solutions

---
+++ 4.1/CashMachine.rb
@@ -0,0 +1,54 @@
+class CashMachine
+  def initialize
+    @balance = 100.0
+  end
+
+  def balance
+    @balance
+  end
+
+  def deposit(value)
+    if value.positive?
+      @balance += value
+    else
+      'Ошибка, сумма меньше или равна нулю.'
+    end
+  end
+
+  def withdraw(value)
+    if value < @balance
+      @balance -= value
+    else
+      'Сумма для вывода больше баланса.'
+    end
+  end
+end
+
+cs = CashMachine.new
+
+loop do
+  puts ''
+  puts 'Вы можете:'
+  puts 'Внести деньги (D)'
+  puts 'Вывести деньги (W)'
+  puts 'Узнать баланс (B)'
+  puts 'Выйти (Q)'
+  puts ''
+
+  case gets.chomp.downcase
+  when 'b'
+    puts "Ваш баланс - #{cs.balance}"
+  when 'd'
+    puts 'Внесите сумму для пополнения'
+    cs.deposit(gets.chomp.to_f)
+
+    puts "Ваш баланс - #{cs.balance}"
+  when 'w'
+    puts 'Внесите сумму для снятия'
+    cs.withdraw(gets.chomp.to_f)
+
+    puts "Ваш баланс - #{cs.balance}"
+  when 'q'
+    break
+  end
+end
