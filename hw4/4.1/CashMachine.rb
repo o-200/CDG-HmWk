7babc38b19520e76c42ae909f65b35b9d7b697ba
Alexander Abramov <omgout200@gmail.com>
2022-12-14 21:31:42 +0700

    corrected

--- hw4/4.1/CashMachine.rb
+++ hw4/4.1/CashMachine.rb
@@ -24,31 +24,33 @@ class CashMachine
   end
 end
 
-cs = CashMachine.new
+def init
+  cs = CashMachine.new
 
-loop do
-  puts ''
-  puts 'Вы можете:'
-  puts 'Внести деньги (D)'
-  puts 'Вывести деньги (W)'
-  puts 'Узнать баланс (B)'
-  puts 'Выйти (Q)'
-  puts ''
+  loop do
+    puts ''
+    puts 'Вы можете:'
+    puts 'Внести деньги (D)'
+    puts 'Вывести деньги (W)'
+    puts 'Узнать баланс (B)'
+    puts 'Выйти (Q)'
+    puts ''
 
-  case gets.chomp.downcase
-  when 'b'
-    puts "Ваш баланс - #{cs.balance}"
-  when 'd'
-    puts 'Внесите сумму для пополнения'
-    cs.deposit(gets.chomp.to_f)
+    case gets.chomp.downcase
+    when 'b'
+      puts "Ваш баланс - #{cs.balance}"
+    when 'd'
+      puts 'Внесите сумму для пополнения'
+      cs.deposit(gets.chomp.to_f)
 
-    puts "Ваш баланс - #{cs.balance}"
-  when 'w'
-    puts 'Внесите сумму для снятия'
-    cs.withdraw(gets.chomp.to_f)
+      puts "Ваш баланс - #{cs.balance}"
+    when 'w'
+      puts 'Внесите сумму для снятия'
+      cs.withdraw(gets.chomp.to_f)
 
-    puts "Ваш баланс - #{cs.balance}"
-  when 'q'
-    break
+      puts "Ваш баланс - #{cs.balance}"
+    when 'q'
+      break
+    end
   end
 end
