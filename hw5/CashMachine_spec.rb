c995bd0f3ef09d9c249df4aa5d945671f8d631d0
Alexander Abramov <omgout200@gmail.com>
2022-12-18 18:19:37 +0700

    refactor tests

--- hw5/CashMachine_spec.rb
+++ hw5/CashMachine_spec.rb
@@ -5,26 +5,34 @@ RSpec.describe CashMachine do
   let(:value) { 25 }
 
   describe '#deposit' do
-    it 'should be a correct deposit' do
-      expect(subject.deposit(value)).to eq 125.0
+    context 'when operation is right' do
+      it 'value is a correct' do
+        expect(subject.deposit(value)).to eq 125.0
+      end
     end
 
-    it 'should be a wrong deposit' do
-      expect { subject.deposit(-1) }.to raise_error('Ошибка, сумма меньше или равна нулю.')
+    context 'when operation is wrong' do
+      it 'value is negative' do
+        expect { subject.deposit(-1) }.to raise_error('Ошибка, сумма меньше или равна нулю.')
+      end
     end
   end
 
   describe '#withdraw' do
-    it 'should be a correct withdraw' do
-      expect(subject.withdraw(value)).to eq 75.0
+    context 'when operation is right' do
+      it 'value is a correct' do
+        expect(subject.withdraw(value)).to eq 75.0
+      end
     end
 
-    it 'should be a wrong withdraw with negative value' do
-      expect { subject.withdraw(-1) }.to raise_error('Ошибка. Сумма для вывода больше баланса или отрицательная')
-    end
+    context 'when operation is wrong' do
+      it 'value is negative' do
+        expect { subject.withdraw(-1) }.to raise_error('Ошибка. Сумма для вывода больше баланса или отрицательная')
+      end
 
-    it 'should be a wrong withdraw with value > balance' do
-      expect { subject.withdraw(1000000) }.to raise_error('Ошибка. Сумма для вывода больше баланса или отрицательная')
+      it 'value > balance' do
+        expect { subject.withdraw(1000) }.to raise_error('Ошибка. Сумма для вывода больше баланса или отрицательная')
+      end
     end
   end
 end
